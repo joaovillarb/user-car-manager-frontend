74a40977f8faa6e1b424f05f3efd43149200d337
João Fagundes Villar Bernardes <36734606+joaovillarb@users.noreply.github.com>
2023-09-23 20:37:13 -0300

    Configuração de guarda de rotas #5 (#6)
    * Criação do authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./features/login/login.component";
 import {HomeComponent} from "./features/home/home.component";
 import {DashboardComponent} from "./features/dashboard/dashboard.component";
+import {authGuard} from "./core/guard/auth.guard";
 
 const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'dashboard', component: DashboardComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
 ];
 
 @NgModule({
