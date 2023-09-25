a462fbc2c3e72bcd51b6a27b234d58b8f1b93226
joaofagundesvillar <joaovillarb@gmail.com>
2023-09-25 18:17:59 -0300

    Adicionando variaveis de ambiente dinamicamente

--- src/app/core/service/account-user.service.ts
+++ src/app/core/service/account-user.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
 import {AccountUser} from "../../shared/model/account-user";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountUserService {
 
-  private readonly baseUrl = 'http://localhost:8080/api';
+  private readonly baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {
   }
--- src/app/core/service/car.service.ts
+++ src/app/core/service/car.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
 import {Car} from "../../shared/model/car";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  private readonly baseUrl = 'http://localhost:8080/api';
+  private readonly baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {
   }
